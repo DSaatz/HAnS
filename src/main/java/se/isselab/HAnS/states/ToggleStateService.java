110f15d713365e325c0ef217c9c7ac4c3c6aa00a
JuliaF9 <152856690+JuliaF9@users.noreply.github.com>
2024-11-06 14:37:00 +0100

    Update ToggleStateService.java

--- src/main/java/se/isselab/HAnS/states/ToggleStateService.java
+++ src/main/java/se/isselab/HAnS/states/ToggleStateService.java
@@ -6,12 +6,22 @@ import com.intellij.openapi.components.Storage;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 @State(
         name = "ToggleState",
         storages = {@Storage("ToggleState.xml")}
 )
 public class ToggleStateService implements PersistentStateComponent<ToggleState> {
+
+    private final PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
     private ToggleState state = new ToggleState();
+
+    public static ToggleStateService getInstance() {
+        return com.intellij.openapi.application.ApplicationManager.getApplication().getService(ToggleStateService.class);
+    }
 
     @Nullable
     @Override
@@ -22,5 +32,23 @@ public class ToggleStateService implements PersistentStateComponent<ToggleState>
     @Override
     public void loadState(@NotNull ToggleState state) {
         this.state = state;
+    }
+
+    public boolean isAnnotationsVisible() {
+        return state.isEnabled;
+    }
+
+    public void setAnnotationsVisible(boolean visible) {
+        boolean oldVisible = state.isEnabled;
+        state.isEnabled = visible;
+        changeSupport.firePropertyChange("annotationsVisible", oldVisible, visible);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.removePropertyChangeListener(listener);
     }
 }
